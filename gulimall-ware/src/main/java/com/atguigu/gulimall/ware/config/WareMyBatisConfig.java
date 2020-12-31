ad01dfe91fab5cbc0383217dee8601738050d3d2
Shaohui Guo <sjph.guo@gmail.com>
2020-12-31 10:18:38 -0800

    bug fix

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/config/WareMyBatisConfig.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/config/WareMyBatisConfig.java
@@ -1,17 +1,9 @@
 package com.atguigu.gulimall.ware.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.zaxxer.hikari.HikariDataSource;
-import io.seata.rm.datasource.DataSourceProxy;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.util.StringUtils;
-
-import javax.sql.DataSource;
 
 //@EnableTransactionManagement
 @MapperScan("com.atguigu.gulimall.ware.dao")
