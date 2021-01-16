8413b73795622dfc22e1aa1ef7ce56c233661f6b
Shaohui Guo <sjph.guo@gmail.com>
2021-01-15 23:02:06 -0800

    test

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,5 +1,6 @@
 package com.atguigu.gulimall.product;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -62,9 +63,11 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableDiscoveryClient
 @MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
+@Slf4j
 public class GulimallProductApplication {
 
     public static void main(String[] args) {
+        log.info("!!!!!!!!!!!!To see start");
         SpringApplication.run(GulimallProductApplication.class, args);
     }
 
